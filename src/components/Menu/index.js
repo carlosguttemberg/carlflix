ef5a3734a7976210d2c78b7c1c5259a1cffec51e
Carlos Guttemberg <guttembergcarlos@gmail.com>
2020-07-28 21:55:21 -0300

    Finalizando a segunda aula

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import Cj from '../../assets/img/Cj.png';
 import './menu.css';
-import Button from '../Button'
+import Button from '../Button';
+import {Link} from 'react-router-dom';
 // import ButtonLink from './components/ButtonLink';
 
 function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="CJ" src={Cj} alt="CarlFlixLogo"></img>
                 <img className="Logo" src={Logo} alt="CarlFlixLogo"></img>
-            </a>
+            </Link>
 
-            <Button as="a" href="/" className="ButtonLink">
+            <Button as={Link} to="/cadastro/video" className="ButtonLink">
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home';
+import CadastroVideo from './Pages/Cadastro/Video';
+import CadastroCategoria from './Pages/Cadastro/Categoria';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} exact/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
+      <Route component={Home}/>
+    </Switch>
+  </BrowserRouter>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
